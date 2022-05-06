f241e30d9687f4d4cd197b6624f4f8b6cca5590a
shinyumi <yumishin43@gmail.com>
2022-05-06 22:36:54 +0900

    server_practice_springboot

---
+++ src/main/java/com/example/demo/src/user/UserDao.java
@@ -0,0 +1,96 @@
+package com.example.demo.src.user;
+
+
+import com.example.demo.src.user.model.DeleteUserReq;
+import com.example.demo.src.user.model.GetUserRes;
+import com.example.demo.src.user.model.PatchUserReq;
+import com.example.demo.src.user.model.PostUserReq;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+//DB에 직접적으로 연결
+@Repository
+public class UserDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource){
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public List<GetUserRes> getUsers(){
+        String getUsersQuery = "select userIdx,name,nickName,email from User";
+        // list 형태의 객체를 반환할 때, query
+        return this.jdbcTemplate.query(getUsersQuery,
+                (rs,rowNum) -> new GetUserRes(
+                        rs.getInt("userIdx"),
+                        rs.getString("name"),
+                        rs.getString("nickName"),
+                        rs.getString("email")
+                ));
+    }
+
+    public GetUserRes getUsersByEmail(String email){
+        String getUsersByEmailQuery = "select userIdx,name,nickName,email from User where email=?";
+        String getUsersByEmailParams = email;
+        return this.jdbcTemplate.queryForObject(getUsersByEmailQuery,
+                (rs, rowNum) -> new GetUserRes(
+                        rs.getInt("userIdx"),
+                        rs.getString("name"),
+                        rs.getString("nickName"),
+                        rs.getString("email")),
+                getUsersByEmailParams);
+    }
+
+
+    // 사용자 인덱스 조회 방식에서 활용
+    public GetUserRes getUsersByIdx(int userIdx){
+        String getUsersByIdxQuery = "select userIdx,name,nickName,email from User where userIdx=?";
+        int getUsersByIdxParams = userIdx;
+        // 하나의 객체로 반환할 때, queryForObject
+        return this.jdbcTemplate.queryForObject(getUsersByIdxQuery,
+                (rs, rowNum) -> new GetUserRes(
+                        rs.getInt("userIdx"),
+                        rs.getString("name"),
+                        rs.getString("nickName"),
+                        rs.getString("email")),
+                getUsersByIdxParams);
+    }
+
+    public int createUser(PostUserReq postUserReq){
+        String createUserQuery = "insert into User (name, nickName, phone, email, password) VALUES (?,?,?,?,?)";
+        Object[] createUserParams = new Object[]{postUserReq.getName(), postUserReq.getNickName(),postUserReq.getPhone(), postUserReq.getEmail(), postUserReq.getPassword()};
+        this.jdbcTemplate.update(createUserQuery, createUserParams);
+
+        String lastInserIdQuery = "select last_insert_id()";
+        return this.jdbcTemplate.queryForObject(lastInserIdQuery,int.class);
+    }
+
+    public int checkEmail(String email){
+        String checkEmailQuery = "select exists(select email from User where email = ?)";
+        String checkEmailParams = email;
+        return this.jdbcTemplate.queryForObject(checkEmailQuery,
+                int.class,
+                checkEmailParams);
+
+    }
+
+    public int modifyUserName(PatchUserReq patchUserReq){
+        String modifyUserNameQuery = "update User set nickName = ? where userIdx = ? ";
+        Object[] modifyUserNameParams = new Object[]{patchUserReq.getNickName(), patchUserReq.getUserIdx()};
+
+        return this.jdbcTemplate.update(modifyUserNameQuery,modifyUserNameParams);
+    }
+
+    public int deleteUser(DeleteUserReq deleteUserReq){
+        String deleteUserQuery = "delete from User where userIdx = ?";
+        int deleteUserParam = deleteUserReq.getUserIdx();
+        return this.jdbcTemplate.queryForObject(deleteUserQuery, int.class, deleteUserParam);
+    }
+
+}
